7390af26724016f3df36ec671a9d4cce787f1be1
Zou Hanya <hanyazou@gmail.com>
2018-05-04 10:48:37 +0900

    Add examples/simple_takeoff.py

---
+++ tellopy/examples/simple_takeoff.py
@@ -0,0 +1,33 @@
+from time import sleep
+import tellopy
+
+
+def handler(event, sender, data, **args):
+    drone = sender
+    if event is drone.CONNECTED_EVENT:
+        print 'connected'
+    elif event is drone.FLIGHT_EVENT:
+        print data
+
+
+def test():
+    drone = tellopy.Tello()
+    try:
+        drone.subscribe(drone.CONNECTED_EVENT, handler)
+        drone.subscribe(drone.FLIGHT_EVENT, handler)
+
+        drone.connect()
+        sleep(2)
+        drone.takeoff()
+        sleep(5)
+        drone.down(50)
+        sleep(5)
+        drone.land()
+        sleep(5)
+    except Exception, ex:
+        print ex
+    finally:
+        drone.quit()
+
+if __name__ == '__main__':
+    test()
