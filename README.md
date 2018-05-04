befc53272ff5075243a014c8d25ae1000ea3c70d
Zou Hanya <hanyazou@gmail.com>
2018-05-04 10:49:33 +0900

    Add README.md and setup files

---
+++ README.md
@@ -0,0 +1,19 @@
+# DJI Tello drone controller python package
+
+This is a python package which controlls DJI toy drone 'Tello'. The major portion of the source
+code was ported from the driver of GOBOT project. For original golang version and protocol in
+detail, please refer their blog post at
+https://gobot.io/blog/2018/04/20/hello-tello-hacking-drones-with-go
+
+![photo](files/tello-and-gamepad.png)
+
+## How to fly
+Please see the API docstring.
+You can find basic usage of this package in example code in the examples folder also.
+```
+bash-3.2$ python
+>>> import tellopy
+>>> help(tellopy)
+Help on package tellopy:
+...
+```
---
+++ setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='tellopy',
+
+    version='0.1.0',
+
+    description='DJI Tello drone controller',
+    url='https://github.com/hanyazou/TelloPy',
+    author='Hanyazou',
+    author_email='hanyazou@gmail.com',
+    python_requires='>=2.7, <3',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: Apache Software License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        # 'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        # 'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='sample development robotics drone',
+
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'files']),
+
+    install_requires=[
+        'louie'
+    ],
+
+    project_urls={
+        'Bug Reports': 'https://github.com/hanyazou/TelloPy/issues',
+        'Say Thanks!': 'https://twitter.com/hanyazou',
+        'Source': 'https://github.com/hanyazou/TelloPy',
+    },
+)
