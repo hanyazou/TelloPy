ce7ab22a18e805f06e7f91d087b4aebafa08ad94
Zou Hanya <hanyazou@gmail.com>
2018-09-09 08:57:57 +0900

    move input event handling from main() to handle_input_event()

--- tellopy/examples/joystick_and_video.py
+++ tellopy/examples/joystick_and_video.py
@@ -110,7 +110,12 @@ class JoystickXONE:
 
 prev_flight_data = None
 video_player = None
-
+buttons = None
+speed = 100
+throttle = 0.0
+yaw = 0.0
+pitch = 0.0
+roll = 0.0
 
 def handler(event, sender, data, **args):
     global prev_flight_data
@@ -140,10 +145,87 @@ def update(old, new, max_delta=0.3):
     return res
 
 
+def handle_input_event(drone, e):
+    global speed
+    global throttle
+    global yaw
+    global pitch
+    global roll
+    if e.type == pygame.locals.JOYAXISMOTION:
+        # ignore small input values (Deadzone)
+        if -buttons.DEADZONE <= e.value and e.value <= buttons.DEADZONE:
+            e.value = 0.0
+        if e.axis == buttons.LEFT_Y:
+            throttle = update(throttle, e.value * buttons.LEFT_Y_REVERSE)
+            drone.set_throttle(throttle)
+        if e.axis == buttons.LEFT_X:
+            yaw = update(yaw, e.value * buttons.LEFT_X_REVERSE)
+            drone.set_yaw(yaw)
+        if e.axis == buttons.RIGHT_Y:
+            pitch = update(pitch, e.value *
+                           buttons.RIGHT_Y_REVERSE)
+            drone.set_pitch(pitch)
+        if e.axis == buttons.RIGHT_X:
+            roll = update(roll, e.value * buttons.RIGHT_X_REVERSE)
+            drone.set_roll(roll)
+    elif e.type == pygame.locals.JOYHATMOTION:
+        if e.value[0] < 0:
+            drone.counter_clockwise(speed)
+        if e.value[0] == 0:
+            drone.clockwise(0)
+        if e.value[0] > 0:
+            drone.clockwise(speed)
+        if e.value[1] < 0:
+            drone.down(speed)
+        if e.value[1] == 0:
+            drone.up(0)
+        if e.value[1] > 0:
+            drone.up(speed)
+    elif e.type == pygame.locals.JOYBUTTONDOWN:
+        if e.button == buttons.LAND:
+            drone.land()
+        elif e.button == buttons.UP:
+            drone.up(speed)
+        elif e.button == buttons.DOWN:
+            drone.down(speed)
+        elif e.button == buttons.ROTATE_RIGHT:
+            drone.clockwise(speed)
+        elif e.button == buttons.ROTATE_LEFT:
+            drone.counter_clockwise(speed)
+        elif e.button == buttons.FORWARD:
+            drone.forward(speed)
+        elif e.button == buttons.BACKWARD:
+            drone.backward(speed)
+        elif e.button == buttons.RIGHT:
+            drone.right(speed)
+        elif e.button == buttons.LEFT:
+            drone.left(speed)
+    elif e.type == pygame.locals.JOYBUTTONUP:
+        if e.button == buttons.TAKEOFF:
+            drone.takeoff()
+        elif e.button == buttons.UP:
+            drone.up(0)
+        elif e.button == buttons.DOWN:
+            drone.down(0)
+        elif e.button == buttons.ROTATE_RIGHT:
+            drone.clockwise(0)
+        elif e.button == buttons.ROTATE_LEFT:
+            drone.counter_clockwise(0)
+        elif e.button == buttons.FORWARD:
+            drone.forward(0)
+        elif e.button == buttons.BACKWARD:
+            drone.backward(0)
+        elif e.button == buttons.RIGHT:
+            drone.right(0)
+        elif e.button == buttons.LEFT:
+            drone.left(0)
+
+
 def main():
+    global buttons
     pygame.init()
     pygame.joystick.init()
-    buttons = None
+
     try:
         js = pygame.joystick.Joystick(0)
         js.init()
@@ -167,87 +249,13 @@ def main():
     drone.start_video()
     drone.subscribe(drone.EVENT_FLIGHT_DATA, handler)
     drone.subscribe(drone.EVENT_VIDEO_FRAME, handler)
-    speed = 100
-    throttle = 0.0
-    yaw = 0.0
-    pitch = 0.0
-    roll = 0.0
 
     try:
         while 1:
             # loop with pygame.event.get() is too much tight w/o some sleep
             time.sleep(0.01)
             for e in pygame.event.get():
-                if e.type == pygame.locals.JOYAXISMOTION:
-                    # ignore small input values (Deadzone)
-                    if -buttons.DEADZONE <= e.value and e.value <= buttons.DEADZONE:
-                        e.value = 0.0
-                    if e.axis == buttons.LEFT_Y:
-                        throttle = update(
-                            throttle, e.value * buttons.LEFT_Y_REVERSE)
-                        drone.set_throttle(throttle)
-                    if e.axis == buttons.LEFT_X:
-                        yaw = update(yaw, e.value * buttons.LEFT_X_REVERSE)
-                        drone.set_yaw(yaw)
-                    if e.axis == buttons.RIGHT_Y:
-                        pitch = update(pitch, e.value *
-                                       buttons.RIGHT_Y_REVERSE)
-                        drone.set_pitch(pitch)
-                    if e.axis == buttons.RIGHT_X:
-                        roll = update(roll, e.value * buttons.RIGHT_X_REVERSE)
-                        drone.set_roll(roll)
-
-                elif e.type == pygame.locals.JOYHATMOTION:
-                    if e.value[0] < 0:
-                        drone.counter_clockwise(speed)
-                    if e.value[0] == 0:
-                        drone.clockwise(0)
-                    if e.value[0] > 0:
-                        drone.clockwise(speed)
-                    if e.value[1] < 0:
-                        drone.down(speed)
-                    if e.value[1] == 0:
-                        drone.up(0)
-                    if e.value[1] > 0:
-                        drone.up(speed)
-                elif e.type == pygame.locals.JOYBUTTONDOWN:
-                    if e.button == buttons.LAND:
-                        drone.land()
-                    elif e.button == buttons.UP:
-                        drone.up(speed)
-                    elif e.button == buttons.DOWN:
-                        drone.down(speed)
-                    elif e.button == buttons.ROTATE_RIGHT:
-                        drone.clockwise(speed)
-                    elif e.button == buttons.ROTATE_LEFT:
-                        drone.counter_clockwise(speed)
-                    elif e.button == buttons.FORWARD:
-                        drone.forward(speed)
-                    elif e.button == buttons.BACKWARD:
-                        drone.backward(speed)
-                    elif e.button == buttons.RIGHT:
-                        drone.right(speed)
-                    elif e.button == buttons.LEFT:
-                        drone.left(speed)
-                elif e.type == pygame.locals.JOYBUTTONUP:
-                    if e.button == buttons.TAKEOFF:
-                        drone.takeoff()
-                    elif e.button == buttons.UP:
-                        drone.up(0)
-                    elif e.button == buttons.DOWN:
-                        drone.down(0)
-                    elif e.button == buttons.ROTATE_RIGHT:
-                        drone.clockwise(0)
-                    elif e.button == buttons.ROTATE_LEFT:
-                        drone.counter_clockwise(0)
-                    elif e.button == buttons.FORWARD:
-                        drone.forward(0)
-                    elif e.button == buttons.BACKWARD:
-                        drone.backward(0)
-                    elif e.button == buttons.RIGHT:
-                        drone.right(0)
-                    elif e.button == buttons.LEFT:
-                        drone.left(0)
+                handle_input_event(drone, e)
     except KeyboardInterrupt as e:
         print(e)
     except Exception as e:
